1ecac1dcfa113f75d223fb1e0f5d5458c92ce9ae
Cristhian S. Schröer <81269993+Cristhian-Shr@users.noreply.github.com>
2025-04-15 20:45:39 -0300

    exclusao de componentes nao utilizados

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,6 @@ import { Navbar } from "../components/headerComponents/Navbar";
 import PhotoAlbum from "@/components/bodyComponentes/AlbumPhotos";
 import Footer from "@/components/footerComponents/Footer";
 import WhatsAppButton from "@/components/bodyComponentes/WhatsAppButton";
-import ProcedurePresentation from "@/components/bodyComponentes/ProcedurePresentation";
-import ProcedurePresentation2 from "@/components/bodyComponentes/ProcedurePresentation2";
-import ProcedurePresentation3 from "@/components/bodyComponentes/ProcedurePresentation3";
 import FacialProcedures from "@/components/bodyComponentes/FacialProcedure";
 import BodyProcedures from "@/components/bodyComponentes/BodyProcedure";
 
@@ -16,9 +13,7 @@ export default function Home() {
       <Navbar />
       <WhatsAppButton />
       <AboutSection />
-      {/* <ProcedurePresentation />
-      <ProcedurePresentation2 />
-      <ProcedurePresentation3 /> */}
+
       <FacialProcedures />
       <BodyProcedures />
       <PhotoAlbum />
