c53ebf164795f3acdf2e159dd15ad32cf192cd72
Cristhian S. Schröer <81269993+Cristhian-Shr@users.noreply.github.com>
2025-04-15 21:37:54 -0300

    menu hamburguer

--- src/components/headerComponents/Navbar.tsx
+++ src/components/headerComponents/Navbar.tsx
@@ -4,28 +4,36 @@ import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import logoTipo from '../../../public/logo/1.png';
-import logoHover from '../../../public/logo/2.png'; 
+import logoHover from '../../../public/logo/2.png';
+import { IoMenu } from "react-icons/io5";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export const Navbar: React.FC = () => {
   const [logo, setLogo] = useState(logoTipo);
 
   return (
-    <div className="w-full flex flex-col md:flex-row items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 h-auto py-4 md:py-6 bg-[linear-gradient(to_right,#c1c3b7,#a1a696,#808b77,#405740,#405740)]">
-      
+    <div className="w-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 py-4 md:py-6 
+    bg-[linear-gradient(to_right,#c1c3b7,#a1a696,#808b77,#405740,#405740)]">
+
       {/* Logo */}
       <div className="bg-primary rounded-full px-2 mb-4 md:mb-0">
-        <Image 
-          alt="logotipo" 
-          src={logo} 
+        <Image
+          alt="logotipo"
+          src={logo}
           className="w-32 sm:w-40 lg:w-52 transition-all duration-300 px-2"
-          onMouseEnter={() => setLogo(logoHover)} 
+          onMouseEnter={() => setLogo(logoHover)}
           onMouseLeave={() => setLogo(logoTipo)}
         />
       </div>
 
-      {/* Menu */}
-      <nav className="text-terciary font-bold flex">
-        <ul className="flex flex-col md:flex-row gap-4 text-center">
+      {/* Menu tradicional (somente tablet e desktop) */}
+      <nav className="hidden md:flex text-terciary font-bold">
+        <ul className="flex gap-6">
           <li className="hover:text-secondary duration-200">
             <Link href="/about">Sobre</Link>
           </li>
@@ -43,6 +51,32 @@ export const Navbar: React.FC = () => {
           </li>
         </ul>
       </nav>
+
+      {/* Dropdown menu (somente no celular) */}
+      <div className="block md:hidden">
+        <DropdownMenu>
+          <DropdownMenuTrigger className="text-5xl text-terciary">
+            <IoMenu />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="bg-primary border-secondary mr-5 w-40 h-48 font-bold text-secondary flex flex-col items-center">
+            <DropdownMenuItem className="py-2 hover:text-terciary w-full text-center ">
+              <Link href="/about">Sobre</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem className="py-2 hover:text-terciary w-full text-center">
+              <Link href="/services">Serviços</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem className="py-2 hover:text-terciary w-full text-center">
+              <Link href="/results">Resultados</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem className="py-2 hover:text-terciary w-full text-center">
+              <Link href="/testimonials">Depoimentos</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem className="py-2 hover:text-terciary w-full text-center">
+              <Link href="/contact">Contato</Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
     </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,6 @@ export default function Home() {
       <Navbar />
       <WhatsAppButton />
       <AboutSection />
-
       <FacialProcedures />
       <BodyProcedures />
       <PhotoAlbum />
