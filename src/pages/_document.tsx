f98bdc70e6ddde6beb8d3ddf882ea05fdc7194d5
Cristhian S. Schröer <81269993+Cristhian-Shr@users.noreply.github.com>
2025-06-13 09:28:44 -0300

    header title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="pt-br">
-      <Head />
+      <Head>
+        <title>Deise Soares | Biomédica Estéta</title>
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
