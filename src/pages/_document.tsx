1be0d79afc4411aba224bce3ebc9e4b68c128427
Cristhian S. Schröer <81269993+Cristhian-Shr@users.noreply.github.com>
2025-06-22 22:02:40 -0300

    remove head em _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,9 +9,6 @@ export const metadata: Metadata = {
 export default function Document() {
   return (
     <Html lang="pt-br">
-      <Head>
-        <title>Deise Soares | Biomédica Estéta</title>
-      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
